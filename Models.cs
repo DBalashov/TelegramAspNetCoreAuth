f99183f97ed5edb7fd900596a2d57cfb5ce5e733
dbalashov <dbalashov@gmail.com>
2024-06-03 22:47:34 +0400

    +bot auth

--- Models.cs
+++ Models.cs
@@ -1,3 +1,11 @@
-﻿namespace TelegramAspNetCoreAuth;
+﻿using System.Text.Json.Serialization;
+
+namespace TelegramAspNetCoreAuth;
 
 public sealed record AuthInfo(Int64 id, string? first_name, string? last_name, string? username, string? photo_url, Int64 auth_date, string hash);
+
+sealed record ErrorResult([property: JsonPropertyName("traceId")]
+                          string TraceId,
+                          [property: JsonPropertyName("title")]  string Title,
+                          [property: JsonPropertyName("status")] int    Status = 400,
+                          [property: JsonPropertyName("type")]   string Type   = "https://tools.ietf.org/html/rfc7231#section-6.5.1");
--- TelegramAuthMiddleware.cs
+++ TelegramAuthMiddleware.cs
@@ -1,93 +1,157 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 
 namespace TelegramAspNetCoreAuth;
 
 public class TelegramAuthMiddleware
 {
     readonly RequestDelegate next;
-
+    
     const string MissingOrInvalid = "'{0}' is missing or invalid";
-
+    
     public TelegramAuthMiddleware(RequestDelegate next) => this.next = next;
-
+    
     public async Task Invoke(HttpContext                     ctx,
                              ILogger<TelegramAuthMiddleware> logger,
                              TelegramAuthConfig              config,
                              ITelegramAuthenticator          authenticator)
     {
-        var info = parseQueryString(ctx.Request.Query, logger);
-        if (info == null || ctx.Request.Method != "GET")
+        if (ctx.Request.Method != "GET")
         {
             ctx.Response.StatusCode = 400;
             return;
         }
-
-        if (!checkHash(config, ctx.Request.Query))
+        
+        if (tryParseNormal(ctx.Request.Query, logger, out var normalAuthInfo))
         {
-            logger.LogWarning("[{0}] {1}: HASH MISMATCH", info.id, info.username);
-            ctx.Response.ContentType = "text/html";
-            ctx.Response.StatusCode  = 200;
-            await ctx.Response.WriteAsync("<html><body><script>alert('Error! Invalid hash!'); window.close();</script></body></html>");
+            if (!checkHash(config, ctx.Request.Query))
+            {
+                logger.LogWarning("[{0}] {1}: HASH MISMATCH", normalAuthInfo.id, normalAuthInfo.username);
+                ctx.Response.ContentType = "text/html";
+                ctx.Response.StatusCode  = 200;
+                await ctx.Response.WriteAsync("<html><body><script>alert('Error! Invalid hash!'); window.close();</script></body></html>");
+                return;
+            }
+            
+            var redirectUrl = await authenticator.Authenticate(normalAuthInfo, ctx);
+            if (redirectUrl != null)
+            {
+                ctx.Response.Redirect(redirectUrl);
+                return;
+            }
         }
-
-        var redirectUrl = await authenticator.Authenticate(info, ctx);
-        if (redirectUrl != null)
+        
+        if (tryParseBot(ctx.Request.Query, logger, out var botAuthInfo))
         {
-            ctx.Response.Redirect(redirectUrl);
+            if (!checkHash(config, ctx.Request.Query, "WebAppData"))
+            {
+                logger.LogWarning("[{0}] {1}: HASH MISMATCH", botAuthInfo.id, botAuthInfo.username);
+                ctx.Response.ContentType = "text/json";
+                ctx.Response.StatusCode  = 400;
+                await ctx.Response.WriteAsync(JsonSerializer.Serialize(new ErrorResult(ctx.TraceIdentifier, "Invalid hash")));
+                return;
+            }
+            
+            var redirectUrl = await authenticator.Authenticate(botAuthInfo, ctx);
+            if (redirectUrl != null)
+            {
+                ctx.Response.StatusCode  = 200;
+                await ctx.Response.WriteAsJsonAsync(new {redirectUrl});
+                return;
+            }
+            
+            ctx.Response.StatusCode  = 401;
+            ctx.Response.ContentType = "text/json";
+            await ctx.Response.WriteAsync(JsonSerializer.Serialize(new ErrorResult(ctx.TraceIdentifier, "Invalid hash")));
             return;
         }
-
+        
         ctx.Response.StatusCode = 400;
         await next(ctx);
     }
-
+    
     #region parseQueryString
-
-    AuthInfo? parseQueryString(IQueryCollection q, ILogger<TelegramAuthMiddleware> logger)
+    
+    bool tryParseNormal(IQueryCollection q, ILogger<TelegramAuthMiddleware> logger, out AuthInfo authInfo)
     {
+        authInfo = null!;
         if (!q.TryGetValue("id", out var _id) || !Int64.TryParse(_id.ToString(), out var id))
         {
-            logger.LogWarning(MissingOrInvalid, "id");
-            return null;
+            logger.LogDebug(MissingOrInvalid, "id");
+            return false;
         }
-
+        
         if (!q.TryGetValue("auth_date", out var _authDate) || !Int64.TryParse(_authDate.ToString(), out var authDate))
         {
-            logger.LogWarning(MissingOrInvalid, "auth_date");
-            return null;
+            logger.LogDebug(MissingOrInvalid, "auth_date");
+            return false;
         }
-
-        if (!q.TryGetValue("hash", out var hash))
+        
+        if (!q.TryGetValue("hash", out var _hash))
         {
-            logger.LogWarning(MissingOrInvalid, "hash");
-            return null;
+            logger.LogDebug(MissingOrInvalid, "hash");
+            return false;
         }
-
-        return new AuthInfo(id,
-                            !q.TryGetValue("first_name", out var _firstName) ? null : _firstName.ToString(),
-                            !q.TryGetValue("last_name",  out var _lastName) ? null : _lastName.ToString(),
-                            !q.TryGetValue("username",   out var _userName) ? null : _userName.ToString(),
-                            !q.TryGetValue("photo_url",  out var _photoUrl) ? null : _photoUrl.ToString(),
-                            authDate,
-                            hash.ToString());
+        
+        authInfo = new AuthInfo(id,
+                                !q.TryGetValue("first_name", out var _firstName) ? null : _firstName.ToString(),
+                                !q.TryGetValue("last_name",  out var _lastName) ? null : _lastName.ToString(),
+                                !q.TryGetValue("username",   out var _userName) ? null : _userName.ToString(),
+                                !q.TryGetValue("photo_url",  out var _photoUrl) ? null : _photoUrl.ToString(),
+                                authDate,
+                                _hash.ToString());
+        return true;
     }
-
+    
+    bool tryParseBot(IQueryCollection q, ILogger<TelegramAuthMiddleware> logger, out AuthInfo authInfo)
+    {
+        authInfo = null!;
+        if (!q.TryGetValue("query_id", out var _query_id))
+        {
+            logger.LogDebug(MissingOrInvalid, "id");
+            return false;
+        }
+        
+        if (!q.TryGetValue("auth_date", out var _authDate) || !Int64.TryParse(_authDate.ToString(), out var authDate))
+        {
+            logger.LogDebug(MissingOrInvalid, "auth_date");
+            return false;
+        }
+        
+        if (!q.TryGetValue("hash", out var _hash))
+        {
+            logger.LogDebug(MissingOrInvalid, "hash");
+            return false;
+        }
+        
+        if (!q.TryGetValue("user", out var _user))
+        {
+            logger.LogDebug(MissingOrInvalid, "user");
+            return false;
+        }
+        
+        authInfo = JsonSerializer.Deserialize<AuthInfo>(_user.ToString())! with {auth_date = authDate, hash = _hash.ToString()};
+        return true;
+    }
+    
     #endregion
-
+    
     #region checkHash
-
-    bool checkHash(TelegramAuthConfig config, IQueryCollection query)
+    
+    bool checkHash(TelegramAuthConfig config, IQueryCollection query, string? key = null)
     {
         ArgumentNullException.ThrowIfNull(query);
-
+        
         var passedHash      = query["hash"];
         var dataCheckString = query.Where(p => p.Key != "hash").Select(p => p.Key + "=" + p.Value).OrderBy(p => p).Aggregate((a, b) => a + "\n" + b);
-        var secretKey       = SHA256.HashData(Encoding.UTF8.GetBytes(config.BotToken));
-
+        var secretKey = key == null
+                            ? SHA256.HashData(Encoding.UTF8.GetBytes(config.BotToken))
+                            : HMACSHA256.HashData(Encoding.UTF8.GetBytes(key), Encoding.UTF8.GetBytes(config.BotToken));
+        
         var calculatedHash = string.Join("", HMACSHA256.HashData(secretKey, Encoding.UTF8.GetBytes(dataCheckString)).Select(p => p.ToString("x2")));
         return string.Compare(passedHash, calculatedHash, StringComparison.OrdinalIgnoreCase) == 0;
     }
-
+    
     #endregion
 }
